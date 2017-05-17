f903bf4933267a4b29ae87bfd2c08fcbb43456d6
Stephen Pegram <spegram91@gmail.com>
2017-05-17 01:55:52 -0400

    added to demonstration in Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,17 +9,20 @@ public class Main {
         book.addEntry("Aurora Banuelos", "1234567890");
         book.addEntry("Chris Kim", "3456789012");
         book.addEntry("Anthony Jones", "5678901234");
+        book.addEntry("Anthony Jones", "7395628174");
         book.addEntry("Justin Haas", "7890123456");
         book.addEntry("Rahmir Ford","9012345678");
         book.addEntry("Rahmir Ford", "4729475801");
 
         System.out.println(book.listAllEntries());
         System.out.println(book.listAllNames());
-        book.removeEntry("Anthony Jones", "5678901234");
+        book.removeRecord("Anthony Jones");
+        book.removeEntry("Rahmir Ford", "(901)-234-5678");
         System.out.println(book.listAllEntries());
         System.out.println(book.listAllNames());
         System.out.println(book.lookUp("Aurora Banuelos"));
         System.out.println(book.reverseLookUp("(901)-234-5678"));
+
 
     }
 }
